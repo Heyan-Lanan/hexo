2b4e51f6d35a122b47839e11427aff92a22ca5e5
{{Heyan-Lanan}} <{{2149730257@qq.com}}>
2022-04-02 20:24:44 +0800

    ok

---
+++ themes/fluid/scripts/injector.js
@@ -0,0 +1,13 @@
+hexo.extend.injector.register(
+    "body_end",
+    `<script src="https://cdn.jsdelivr.net/gh/Heyan-Lanan/images@main/banner.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/Heyan-Lanan/images@main/snow.js"></script>
+    <link defer rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Heyan-Lanan/images@main/banner.css" />
+    `
+
+);
+
+
+
+
+
